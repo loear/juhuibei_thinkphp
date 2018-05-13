dad777737b3405cf8dda893f04041885e37500d7
Amos <web@loear.com>
2018-05-13 13:38:50 +0800

    Course

---
+++ application/api/controller/c1/Course.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guo
+ * Date: 2018/5/13
+ * Time: 10:11
+ */
+
+namespace app\api\controller\c1;
+
+
+class Course
+{
+    public function getClassData()
+    {
+
+    }
+
+}
